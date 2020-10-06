2bf500b4bc1efd2003416647237a5b7fbf02efea
vedant jawanjal <66886329+chotudevil@users.noreply.github.com>
2020-10-06 10:58:25 +0700

    Dynamic Resume Update

--- Validation.js
+++ Validation.js
@@ -1,57 +1,25 @@
-let firstName = document.getElementById("firstName");
-let middleName = document.getElementById("middleName");
-let lastName = document.getElementById("lastName");
-let emailId = document.getElementById("email-id");
-let phoneNo = document.getElementById("phone-no");
 
 
+function onUpdate(fieldname ,e){
+  var inputText = e.value; 
 
+  if(fieldname == "fullname"){    
+    document.getElementById("ful-name").innerText = inputText;
+  }
 
- document.getElementById("firstName").addEventListener("keyup", function() {
-    firstName = document.getElementById("firstName").value;
-    if (firstName.length == 0){
-        document.getElementById("first-error").classList.remove("no-error");
-        document.getElementById("first-error").classList.add("error");       
-    }
-    else{
-        document.getElementById("name-display").innerHTML = firstName;
-        document.getElementById("first-error").classList.remove("error");
-        document.getElementById("first-error").classList.add("no-error");
-    }
-   
-  });
-
-  document.getElementById("middleName").addEventListener("keyup", function() {
-    middleName = document.getElementById("middleName").value;
-    if (middleName.length == 0){
-        document.getElementById("middle-error").classList.remove("no-error");
-        document.getElementById("middle-error").classList.add("error");
-    }else{
-        document.getElementById("name-display").innerHTML = firstName +" "+  middleName;
-        document.getElementById("middle-error").classList.remove("error");
-        document.getElementById("middle-error").classList.add("no-error");
-    }
-   
-  });
-  document.getElementById("lastName").addEventListener("keyup", function() {
-    lastName = document.getElementById("lastName").value;
-    if(lastName == 0){
-        document.getElementById("last-error").classList.remove("no-error");
-        document.getElementById("last-error").classList.add("error");
-    }
-    else{
-        document.getElementById("name-display").innerHTML = firstName +" "+  middleName + " " + lastName;
-        document.getElementById("last-error").classList.remove("error");
-        document.getElementById("last-error").classList.add("no-error");
-    }
-   
-  });
-
-
-//   function checkError( index ){
-//     var arr = ['"first-error"','"middle-error"','"last-error"'];
-//     var showclass = arr[index];
-//     document.getElementById(arr[index]).classList.remove("no-error");
-//     document.getElementById(arr[index]).classList.add("error");
-// }
-
+  if(fieldname == "designation"){    
+    document.getElementById("designation").innerText = inputText;
+  }
+  if(fieldname == "desc"){    
+    document.getElementById("description").innerText = inputText;
+  }
+  if(fieldname == "contact"){    
+    document.getElementById("contact").innerText = inputText;
+  }
+  if(fieldname == "EmailID"){    
+    document.getElementById("email").innerText = inputText;
+  }
+  if(fieldname == "Address"){    
+    document.getElementById("address").innerText = inputText;
+  }
+}
